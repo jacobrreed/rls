2d13c27a1178df555b681732db0501c95442b609
Jacob Reed <jacob.russell.reed@gmail.com>
2023-04-18 22:13:07 -0400

    test

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,52 @@
 extern crate url;
+use harsh::Harsh;
 use url::{Url};
 use axum::{http::StatusCode, response::IntoResponse, routing::post, Json, Router};
-use serde::Deserialize;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use tower_http::cors::{Any, CorsLayer};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-#[derive(Deserialize)]
-struct CreateUrl {
-    url: String,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, Eq, Hash, PartialEq)]
+#[derive(Deserialize, Serialize)]
 struct UrlRes {
     url: String,
-    shortUrl: String,
 }
+
+const SALT: &str = "rls";
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "axum_api=debug".into()),
-        ))
-        .with(tracing_subscriber::fmt::layer())
-        .init();
-
     let cors = CorsLayer::new().allow_origin(Any);
 
     let app = Router::new().route("/", post(generate_url)).layer(cors);
 
     let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 8080));
-    tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .expect("failed to start server");
 }
 
-async fn generate_url(Json(payload): Json<CreateUrl>) -> impl IntoResponse {
-    // check if url is valid
-    let url = Url::parse(&payload.url).unwrap();
-    let url_res = UrlRes {
-        url: url.to_string(),
-        shortUrl: String::from("https://neonvoid.io/{}"),
-    };
+// post request that handles generating a short url
+async fn generate_url(Json(payload): Json<UrlRes>) -> (axum::http::StatusCode, axum::Json<UrlRes>) {
 
-    (StatusCode::CREATED, Json(url_res))
 }
+
+
+// function that generates a short url hash given a string
+fn generate_hash(url: &str) -> String {
+  let harsh = Harsh::builder().salt(SALT).build().unwrap();
+  return harsh.encode(&[url.len() as u64]);
+}
+
+// create api that takes POST request
+// post request takes url
+// -> check if URL exists in db (lookup hash)
+//     if it does:
+//       return 200 with short url
+//     if it doesnt:
+//       generate a hash for short url
+//       store hash in db with long url as value
+//       return 201 (create) with short url
+// get request:
+//   takes hash from url "example.com/THISISTHEHASH"
+//   lookup hash in db
+//   redirect to long url (or return) (301?)
--- src/storage.rs
+++
@@ -1,7 +0,0 @@
-use std::collections::HashMap;
-use main::generate_url;
-
-pub struct Storage {
-    urls: HashMap<String, String>,
-    shortener: Shortener
-}
